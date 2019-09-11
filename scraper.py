26a86874004a7cc0c2c3a9a33686b1a0b2b178fe
jwsmith3379 <52710554+jwsmith3379@users.noreply.github.com>
2019-09-11 12:25:22 -0500

    Updated MLB Scraper

--- scraper.py
+++ scraper.py
@@ -1,23 +1,30 @@
 import datetime
-import re
 from datetime import timedelta
 
 import requests
-from bs4 import BeautifulSoup as bs
+import statsapi
+from bs4 import BeautifulSoup
+
+
+class Scraper:
+    def __init__(self, url):
+        self.soup = self.get_soup(url)
+        pass
+
+    def get_soup(self, url):
+        req = requests.get(url)
+        return BeautifulSoup(req.content, features='lxml')
 
 
 def power_rankings(link_to_site):
-    """Use requests library to extract weekly ESPN MLB Power Rankings for use in Team Watchability Index calculation.
-
+    """ Use requests library to extract weekly ESPN MLB Power Rankings for use in Team Watchability Index calculation.
     Args:
         link_to_site: url to ESPN MLB Power Rankings website ('https://www.espn.com/mlb/powerrankings')
-
     Returns:
-        {Date : {Team_1:Ranking, Team_2:Ranking, Team_3:Ranking, ..., Team_30:Ranking}}
+        {Date: {Team_1: Ranking, Team_2: Ranking, Team_3: Ranking, ..., Team_30: Ranking}}
     """
     # Retrieves section of website containing team rankings
-    req = requests.get(link_to_site)
-    soup = bs(req.content, features='lxml')
+    soup = Scraper(url=link_to_site).soup
     power_ranking_team_list = soup.find_all('ol')[0]
     team_rankings = {}
 
@@ -32,128 +39,132 @@ def power_rankings(link_to_site):
     return dict_of_rankings
 
 
-def team_records(link_to_site):
-    """Use requests library to extract weekly ESPN MLB Team Records for use in Team Watchability Index calculation.
-
-    Args:
-        link_to_site: url to ESPN MLB Power Rankings website ('https://www.espn.com/mlb/powerrankings')
+def team_records():
+    """ Use MLB Stats API to extract daily MLB Team Records for use in Game Watchability Index calculation
 
     Returns:
-        {Date : {Team_1 : {Wins:##, Losses:##}, Team_2 : {Wins:##, Losses:##}, ..., Team_30 : {Wins:##, Losses:##}}
+        {Date: {Team_Name: (Wins, Losses), Team_Name: (Wins, Losses), ..., Team_Name: (Wins, Losses)}}
     """
-    # Retrieves section of website containing team rankings
-    req = requests.get(link_to_site)
-    soup = bs(req.content, features='lxml')
-    records_team_list = soup.find_all('ol')[0]
-    team_records_raw = {}
+    # Retrieve today's league standings
+    today = datetime.datetime.today().strftime('%Y-%m-%d')
+    standings = statsapi.standings_data()
 
-    # Retrieves team record (##-## format), splits into wins and losses,
-    # and updates inner dict containing wins and losses
-    for count, item in enumerate(records_team_list.contents):
-        team_name = item.a.text
-        team_record = item.find_all('br')[1].previous_element
-        wins, losses = team_record.split('-')
-        wins_and_losses = {'Wins': int(wins), 'Losses': int(losses)}
-        team_records_raw.update({team_name: wins_and_losses})
+    # League Codes:
+    # 200 - AL West
+    # 201 - AL East
+    # 202 - AL Center
+    # 203 - NL West
+    # 204 - NL East
+    # 205 - NL Central
 
-    # Creates outer dict using {date : wins_and_losses} format
-    dict_of_records = {datetime.datetime.today().strftime('%m-%d-%Y'): team_records_raw}
+    # Use League Codes to extract team name, wins, and losses
+    records = {}
+    for league in range(200, 206):
+        for team in range(len(standings[league]['teams'])):
+            teams = standings[league]['teams'][team]['name']
+            wins = standings[league]['teams'][team]['w']
+            losses = standings[league]['teams'][team]['l']
 
-    return dict_of_records
+            records.update({teams: (wins, losses)})
+
+    # Create main dictionary using date as key and records dictionary as value
+    records_dict = {today: records}
+
+    return records_dict
 
 
-def team_schedule(link_to_site):
-    """Use requests library to extract daily MLB Team Schedules for use in Game Watchability Index calculation
-
-    Args:
-        link_to_site: url to Sports Illustrated MLB Schedule website ('https://www.si.com/mlb/schedule')
+def team_schedule():
+    """ Use MLB Stats API to extract daily MLB Team Schedules for use in Game Watchability Index calculation
 
     Returns:
-        {Date : {Home 1: Team_Name, Away 1: Team_Name, ..., Home 8: Team_Name, Away 8: Team_Name}
+        {Date: {H1: Team_Name, A1: Team_Name, ..., H8: Team_Name, A8: Team_Name}}
     """
-    # Retrieves section of website containing today's schedule
-    req = requests.get(link_to_site)
-    soup = bs(req.content, features='lxml')
-    schedule_team_list = soup.find('div', attrs={'class': re.compile('component schedule')})
+    # Retrieve schedule of today's games
+    today = datetime.datetime.today().strftime('%Y-%m-%d')
+    schedule = statsapi.schedule(today)
+
+    # Team Codes:
+    # 108	LAA	Angels
+    # 109	ARI	D-backs
+    # 110	BAL	Orioles
+    # 111	BOS Red Sox
+    # 112	CHC Cubs
+    # 113	CIN	Reds
+    # 114	CLE	Indians
+    # 115	COL	Rockies
+    # 116	DET	Tigers
+    # 117	HOU Astros
+    # 118	KC	Royals
+    # 119	LAD	Dodgers
+    # 120	WSH Nationals
+    # 121	NYM Mets
+    # 133	OAK Athletics
+    # 134	PIT	Pirates
+    # 135	SD	Padres
+    # 136	SEA	Mariners
+    # 137	SF	Giants
+    # 138	STL	Cardinals
+    # 139	TB	Rays
+    # 140	TEX	Rangers
+    # 141	TOR	Blue Jays
+    # 142	MIN Twins
+    # 143	PHI	Phillies
+    # 144	ATL	Braves
+    # 145	CWS White Sox
+    # 146	MIA	Marlins
+    # 147	NYY	Yankees
+    # 158	MIL	Brewers
+
+    # Convert Team Codes into Abbreviations and use as new dictionary values
     daily_schedule = {}
+    for team in range(len(schedule)):
+        home_team = statsapi.lookup_team(schedule[team]['home_id'])[0]['fileCode'].upper()
+        away_team = statsapi.lookup_team(schedule[team]['away_id'])[0]['fileCode'].upper()
+        daily_schedule.update({'H' + str(team): home_team,
+                               'A' + str(team): away_team})
 
-    # Retrieves team abbreviations in Away-Home alternating format, then splits overall list into
-    # Home and Away lists for the final dictionary
-    raw_text = [row.text for row in schedule_team_list.find_all('span', attrs={'class': re.compile('numeric-score')})]
-    raw_team_abbreviations = [re.findall(r'\w+', raw_text[i]) for i in range(0, len(raw_text), 2)]
-    home_teams = [raw_team_abbreviations[1::2][i][0].upper() for i in range(int(len(raw_team_abbreviations) / 2))]
-    away_teams = [raw_team_abbreviations[0::2][i][0].upper() for i in range(int(len(raw_team_abbreviations) / 2))]
+    # Append previous dictionary to main dictionary
+    team_schedules = {today: daily_schedule}
 
-    # Updates inner dictionary with Home and Away teams to become values for date key
-    for team in range(len(home_teams)):
-        daily_schedule.update({'Home ' + str(team): home_teams[team],
-                               'Away ' + str(team): away_teams[team]})
-
-    # Creates outer dict using {date : daily_schedule} format
-    dict_of_schedule = {datetime.datetime.today().strftime('%m-%d-%Y'): daily_schedule}
-
-    return dict_of_schedule
+    return team_schedules
 
 
-def box_scores(link_to_site):
-    """Use requests library to extract daily MLB box scores for use in Live Game Watchability Index calculation
-
-    Args:
-        link_to_site: url to Sports Illustrated MLB Scoreboard website ('https://www.si.com/mlb/scoreboard')
+def box_scores():
+    """ Use MLB Stats API to extract daily MLB box scores for use in Live Game Watchability Index calculation
 
     Returns:
-        {Date : {Home-Away Concatenation: {H0: [Box Score], A0: [Box Score]}, ...
-         , Home-Away Concatenation: {H8: [Box Score], A8: [Box Score]}}}
+        {Date: {Home-Away First Code: (Home Score, Away Score), ..., Home-Away Last Code: (Home Score, Away Score)}}
     """
-    # Define team names for translation to abbreviations
-    team_dict = {"Orioles": "BAL", "Red Sox": "BOS", "Yankees": "NYY", "Rays": "TB", "Blue Jays": "TOR",
-                 "White Sox": "CWS", "Indians": "CLE", "Tigers": "DET", "Royals": "KC", "Twins": "MIN",
-                 "Astros": "HOU", "Angels": "LAA", "Athletics": "OAK", "Mariners": "SEA", "Rangers": "TEX",
-                 "Braves": "ATL", "Marlins": "MIA", "Mets": "NYM", "Phillies": "PHI", "Nationals": "WAS",
-                 "Cubs": "CHC", "Reds": "CIN", "Brewers": "MIL", "Pirates": "PIT", "Cardinals": "STL",
-                 "Diamondbacks": "ARI", "Rockies": "COL", "Dodgers": "LAD", "Padres": "SD", "Giants": "SF"}
+    # Create dynamic variable to get yesterday's box scores
+    yesterday = (datetime.datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+    schedule = statsapi.schedule(yesterday)
 
-    # Retrieves section of website box scores and team names
-    req = requests.get(link_to_site)  # CHANGE
-    soup = bs(req.content, features='lxml')
-    scores = soup.find('div', attrs={'class': re.compile('content padding')})
-    daily_scores = {}
+    # Retrieve values from schedule dictionary
+    game_info = [list(schedule[i].values()) for i in range(len(schedule))]
 
-    # Extract box score values for each team from each game
-    box = [row.text for row in scores.find_all('tr', attrs={'class': re.compile('is-')})]
-    box_score_values = [re.findall(r'\d+', box[i]) for i in range(len(box))]
+    # Extract game codes from schedule dictionary values
+    game_codes = [game_info[i][0] for i in range(len(game_info))]
 
-    # Append 0s to innings that were canceled/didn't happen (i.e., bottom of 9th)
-    for i in range(len(box_score_values)):
-        while len(box_score_values[i]) != 12:
-            box_score_values[i].insert(-3, '0')
+    # Use game codes to retrieve all info for correct games
+    home_away = []
+    for i in range(len(game_codes)):
+        home_away.append(statsapi.boxscore_data(game_codes[i]))
 
-    # Extract team names from each game
-    team_names = [row.text for row in scores.find_all('span', attrs={'class': re.compile('team-name')})]
-    teams = team_names[1::2]
+    # Use game codes to retrieve box scores for correct games
+    boxes = []
+    for i in range(len(game_codes)):
+        home_box = (statsapi.linescore(game_codes[i]).splitlines()[2])
+        away_box = (statsapi.linescore(game_codes[i]).splitlines()[1])
+        boxes.extend([home_box, away_box])
 
-    # Populate postponed games with 999 for each inning to allow them to stay in
-    # the final dict but not be calculated on
-    postponed = [999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999]
-    if len(box_score_values) != len(teams):
-        diff = len(teams) - len(box_score_values)
-        for i in range(diff):
-            box_score_values.append(postponed)
+    # Create dictionary with Home-Away concatenation as key and scores as values
+    box_score = {}
+    for i in range(len(home_away)):
+        home_away_codes = (list(home_away[i]['teamInfo']['home'].values())[1] +
+                           list(home_away[i]['teamInfo']['away'].values())[1])
+        box_score.update({home_away_codes: (boxes[i * 2], boxes[(i * 2) + 1])})
 
-    # Convert team names to abbreviations
-    games = [team_dict[i] for i in teams]
+    # Create main dictionary with date as key and box score dictionary as value
+    score_dict = {yesterday: box_score}
 
-    # Concatenate Home + Away to create unique codes for each game
-    game_codes = [games[i + 1] + games[i] for i in range(0, len(games), 2)]
-
-    # Create inner dict of Home and Away box scores to become values in outer dict
-    for score in range(0, len(box_score_values), 2):
-        box_values = ({'Home': box_score_values[score + 1], 'Away': box_score_values[score]})
-        daily_scores.update({game_codes[round(score / 2)]: box_values})
-
-    # Create outer dict using {yesterday's date : daily_scores} format because
-    # box scores only exist for games already played
-    yesterday = (datetime.datetime.today() - timedelta(days=1)).strftime('%m/%d/%Y')
-    dict_of_box_scores = {yesterday: daily_scores}
-
-    return dict_of_box_scores
+    return score_dict
